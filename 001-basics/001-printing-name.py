f37748058c48169eafaebd02b455bfbed9c3661b
Manikandan Ramanathan <mramana2@cisco.com>
2023-09-16 18:55:57 +0530

    Basic

---
+++ 001-basics/001-printing-name.py
@@ -0,0 +1,11 @@
+print ("Instead of Empty Line")
+
+# print - Function's name
+# () - Placeholder for Argument
+# () - Empty value is permitted
+# (a, b, c) - Multiple Values are Permitted
+
+print ("Hello June", "Hello Akrti")
+
+c = print ("Hello June")
+print(c)
